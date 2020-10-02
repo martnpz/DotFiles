fbe3b8d2b5d40e889a99116fe10baecb3efcb6b6
martnpz <martinmatiasperez2015@gmail.com>
2020-10-02 16:38:57 -0300

    install file

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Alacritty
+ln -s $(pwd)/.config/alacritty/alacritty.yml $HOME/.config/
+# Neofetch
+ln -s $(pwd)/.config/neofetch $HOME/.config/
+# Ranger
+ln -s $(pwd)/.config/ranger $HOME/.config/
+# Tmux
+ln -s $(pwd)/.config/tmux/* $HOME/
+# Vim
+ln -s $(pwd)/.config/vim/* $HOME/
+# Zsh
+ln -s $(pwd)/.config/zsh/* $HOME/
